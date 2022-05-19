358c5329f30d22d0a37d510841da69170a479f1e
Floyd Remmerswaal <floydremmerswaal@gmail.com>
2022-05-19 20:22:45 +0200

    add pcg folder

---
+++ pcg/pcg.py
@@ -0,0 +1,33 @@
+
+
+class Entity():
+    def __init__(team, posx, posy, orientation):
+        
+        pass
+
+class pcg():
+
+    entityList = []
+
+    def __init__():
+        pass
+
+    def addEntity2(self, posx, posy, orientation)
+
+    def addEntity(self, entity):
+        self.entityList.append(entity)
+
+    def addCavalryArcher(self, x, posx, posy, orientation, team = 1):
+        
+
+    def addSpearman(self, posx, posy, orientation, team = 2):
+        pass
+
+    def toString(self):
+        pass
+
+    def printXML(self):
+        pass
+
+    def write(filename):
+        pass
