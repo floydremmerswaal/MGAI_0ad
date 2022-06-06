aed068f5ca5a2533cb1a2646b1681e45551bed0c
Ernest Vanmosuinck <ernestvmo@gmail.com>
2022-06-06 12:42:58 +0200

    Added function to build map with maze and troops

--- pcg/main.py
+++ pcg/main.py
@@ -1,4 +1,5 @@
 from pcg import PCG
+from maze import Maze, init # remove init() when done debugging
 
 # separated by wall
 def cavalryVsInfantry():
@@ -54,9 +55,50 @@ def cavalryVsInfantryF2():
 
     builder.write("CavalryVsSpearmanFort.xml")
 
+def cavalryVsInfantryMaze(maze_height, maze_width):
+    builder = PCG()
+    x = 483.3144
+    z = 116.94448
+
+    WALL_LENGTH = 25.6
+    ORIENTATION_1 = 1.597
+    ORIENTATION_2 = 4.65592
+    
+    maze = Maze(maze_height, maze_width)
+    maze.generate()
+    # maze.print_maze()
+
+    flag = False
+
+    for i in range(maze_height):
+        for j in range(maze_width):
+            if maze.maze[i][j] == maze.wall:
+                builder.addWall((x + (i*WALL_LENGTH)), ((z + (j*WALL_LENGTH)) - WALL_LENGTH / 2), orientation=0)
+                builder.addWall(((x + (i*WALL_LENGTH)) - WALL_LENGTH / 2), (z + (j*WALL_LENGTH)), orientation=ORIENTATION_1)
+                builder.addWall((x + (i*WALL_LENGTH)), ((z + (j*WALL_LENGTH)) + WALL_LENGTH / 2), orientation=0)
+                builder.addWall(((x + (i*WALL_LENGTH)) + WALL_LENGTH / 2), (z + (j*WALL_LENGTH)), orientation=ORIENTATION_1)
+            if maze.maze[i][j] == maze.exit:
+                if not flag:
+                    builder.addCavalryArcher((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) + 3, orientation=ORIENTATION_1)
+                    builder.addCavalryArcher((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) + 1, orientation=ORIENTATION_1)
+                    builder.addCavalryArcher((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) - 1, orientation=ORIENTATION_1)
+                    builder.addCavalryArcher((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) - 3, orientation=ORIENTATION_1)
+                    flag = True
+                else:
+                    builder.addSpearman((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) + 3, orientation=ORIENTATION_2)
+                    builder.addSpearman((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) + 1, orientation=ORIENTATION_2)
+                    builder.addSpearman((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) - 1, orientation=ORIENTATION_2)
+                    builder.addSpearman((x + (i*WALL_LENGTH)), (z + (j*WALL_LENGTH)) - 3, orientation=ORIENTATION_2)
+    
+    # reinforce exterior wall so troops stay in maze
+    for j in range(maze_width):
+        builder.addWall((x - (WALL_LENGTH / 2 + 5)), (z + (j*WALL_LENGTH)), orientation=ORIENTATION_1)
+        builder.addWall(((x + (maze_height - 1) * WALL_LENGTH) + (WALL_LENGTH / 2 + 5)), (z + (j*WALL_LENGTH)), orientation=ORIENTATION_1)
+
+    builder.write("CavalryVsInfantryMaze.xml")
+
 
 if __name__ == '__main__':
     print("Building")
-    # cavalryVsInfantry()
-    cavalryVsInfantryF1()
+    cavalryVsInfantryMaze(maze_height=6, maze_width=7)
     print("Done.")
