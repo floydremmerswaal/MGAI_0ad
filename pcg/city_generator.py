8c5596a5fe6ae402f0805d6c7463a3e00721bdc1
Sava <savasharif@gmail.com>
2022-06-08 22:08:49 +0200

    Add rashids polygon code

--- pcg/city_generator.py
+++ pcg/city_generator.py
@@ -2,6 +2,8 @@
 import math
 from typing import List
 from matplotlib import pyplot as plt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 import numpy as np
 
@@ -22,6 +24,12 @@ class City():
 
     def build_short_wall(self, coord, orientation):
         self.builder.addBareEntity(entitytype=entities.skirmish__structures__default_wall_short, team=0, posx=coord[0], posz=coord[1], orientation=orientation)
+        
+    def build_gate(self, coord, orientation):
+        self.builder.addBareEntity(entitytype=entities.skirmish__structures__default_wall_gate, team=0, posx=coord[0], posz=coord[1], orientation=orientation)
+
+    def build_watch_tower(self, coord, orientation):
+        self.builder.addBareEntity(entitytype=entities.skirmish__structures__default_wall_tower, team=0, posx=coord[0], posz=coord[1], orientation=orientation)
 
     def generate_circle(self, radius):
         # compute circumference of circle with radius
@@ -35,9 +43,6 @@ class City():
             rads = math.atan2(direction[0], direction[1])
             
             self.build_short_wall(p, rads)
-
-    def build_watch_tower(self, coord, orientation):
-        self.builder.addBareEntity(entitytype=entities.skirmish__structures__default_wall_tower, team=0, posx=coord[0], posz=coord[1], orientation=orientation)
         
     def generate_watch_towers_circle(self, radius):
         # compute circumference of circle with radius
@@ -94,12 +99,14 @@ class City():
         self.generate_watch_towers_circle(self.radii[1])
 
         # outer city
-        district_centers = self.generate_districts(self.radii[1], self.radii[0], no_districts=10)
-        self.generate_district_boundaries(self.radii[1], self.radii[0], district_centers)
+        outer_centers = self.generate_districts(self.radii[1], self.radii[0], no_districts=10)
+        vor = self.generate_district_boundaries(self.radii[1], self.radii[0], outer_centers)
         
         # inner city
-        district_centers2 = self.generate_districts(self.radii[0], 0, no_districts=1, no_highways=10)
-        self.generate_district_boundaries(self.radii[0], 0, district_centers2)
+        inner_centers = self.generate_districts(self.radii[0], 0, no_districts=1, no_highways=10)
+        self.generate_district_boundaries(self.radii[0], 0, inner_centers)
+        
+        self.generate_structures_in_district_polygon(vor, outer_centers)
 
 
     def generate_district_boundaries(self, outer_radius, inner_radius, district_centers):
@@ -141,6 +148,7 @@ class City():
             to_point = vor.vertices[ridge_to]
 
             self.build_wall_line(from_point, to_point, inner_radius, outer_radius)
+        return vor
 
     def generate_districts(self, outer_radius, inner_radius, no_districts, no_highways=3):
         district_centers = []
@@ -159,3 +167,41 @@ class City():
             self.builder.addBareEntity(entitytype=entities.structures__maur__tower_double, team=0, posx=district[0], posz=district[1], orientation=2.35621)
         return district_centers
             
+    # get polygon coordinates from the voronoi regions
+    def generate_polygons_coords_from_voronoi(self, vor):
+        polygons = {}
+        for id, region_index in enumerate(vor.point_region):
+            if -1 in vor.regions[region_index]:
+                continue
+
+            points = []
+            for vertex_index in vor.regions[region_index]:
+                points.append(tuple(vor.vertices[vertex_index]))
+            points.append(points[0])
+            polygons[id] = Polygon(points)
+        return polygons
+
+    def generate_structures_in_district_polygon(self, vor, district_centers):
+        polygons = self.generate_polygons_coords_from_voronoi(vor)
+        range = 20
+        step = 5
+        
+        for key, polygon in polygons.items():
+            district = district_centers[key]
+            occupied_coords = []
+            country = np.random.choice(['spart'])
+            # print("checking new polygon", key) 
+            rand_offset_count = 0
+            struct_candidates = [item for item in dir(entities) if item.startswith("structures__" + country) and not "wall" in item]
+            while len(occupied_coords) < 6: # until atleast 6 structures are built
+                rand_offset = np.random.uniform(-20, 20)
+                #hacky, if no random placement coord are found in the polygon after a certain values skip rest of the polygon
+                if rand_offset_count == (2 * range / step): 
+                    break
+                placement_coord = (district[0] + rand_offset, district[1] + rand_offset)
+                if polygon.contains(Point(placement_coord)) and not placement_coord in occupied_coords:
+                    # print("adding structures at", placement_coord)
+                    rand_structure = np.random.choice(struct_candidates)
+                    self.builder.addBareEntity(entities.__dict__.get(rand_structure), team=0, posx=placement_coord[0], posz=placement_coord[1], orientation=2.35621)
+                    occupied_coords.append(placement_coord)
+                rand_offset_count += 1 
