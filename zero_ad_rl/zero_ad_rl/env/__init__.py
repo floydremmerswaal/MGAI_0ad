dc47a0b510490a96400eb155d1a9bd57420ee3f9
ernestvmo <ernestvmo@gmail.com>
2022-06-09 20:15:43 +0200

    Update environment registration

--- zero_ad_rl/zero_ad_rl/env/__init__.py
+++ zero_ad_rl/zero_ad_rl/env/__init__.py
@@ -6,4 +6,6 @@ def register_envs():
     register_env('CavalryVsSpearmen', lambda c: CavalryVsSpearmenEnv(c))
     register_env('SimpleMinimapCavVsInf', lambda c: SimpleMinimapCavVsInfEnv(c))
     register_env('MinimapCavVsInf', lambda c: MinimapCavVsInfEnv(c))
+    register_env('CavalryVsInfantryMaze', lambda c: CavalryVsInfantryMazeEnv(c))
+    register_env('CavalryVsInfantryCity', lambda c: CavalryVsInfantryMazeEnv(c))
 
